1b09980beb22fb52aec92b2a26ee167e89e957ea
Alexander Borovskikh <borislav_god@mail.ru>
2019-05-20 20:23:44 +0500

    Добавьте файлы проекта.

---
+++ chessboard/CellView.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace chessboard
+{
+    /// <summary>
+    /// Логика взаимодействия для CellView.xaml
+    /// </summary>
+    public sealed partial class CellView : UserControl
+    {
+        private readonly Border _cell;
+        private TextBlock _innerText;
+        private Brush _color;
+
+        public CellView()
+        {
+            InitializeComponent();
+
+            _cell = new Border() { Name = "Cell" };
+
+            _innerText = new TextBlock()
+            {
+                FontSize = 50d,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Name = "Number"
+            };
+
+            _cell.Child = _innerText;
+            this.AddChild(_cell);
+
+        }
+
+        public static readonly DependencyProperty IsSetProperty = 
+            DependencyProperty.Register(nameof(IsSet), typeof(bool), typeof(CellView),
+                new PropertyMetadata(false, delegate (DependencyObject o, DependencyPropertyChangedEventArgs args)
+                {
+                    if (o is CellView cv)
+                        if (args.NewValue is bool value) return;
+                            
+                }));
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(nameof(Color), typeof(Brush), typeof(CellView),
+                new PropertyMetadata(Brushes.Wheat, delegate (DependencyObject o, DependencyPropertyChangedEventArgs args)
+                {
+                    if (o is CellView cv)
+                        if (args.NewValue is Brush color)
+                        {
+                            cv._cell.Background = color;
+                            cv._color = cv._color ?? color;
+                        }
+                }));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(CellView),
+                new PropertyMetadata(default, delegate (DependencyObject o, DependencyPropertyChangedEventArgs args)
+                {
+                    if (o is CellView cv)
+                        if (args.NewValue is string text)
+                            cv._innerText.Text = text;
+                }));
+
+        public static readonly DependencyProperty IsHorseProperty =
+            DependencyProperty.Register(nameof(IsHorse), typeof(bool), typeof(CellView),
+                new PropertyMetadata(false, delegate (DependencyObject o, DependencyPropertyChangedEventArgs args)
+                {
+                    if (o is CellView cv)
+                        if (args.NewValue is bool value)
+                            cv.Color = value ? Brushes.GreenYellow : cv._color;
+                }));
+
+        public static readonly DependencyProperty XProperty =
+            DependencyProperty.Register(nameof(X), typeof(int), typeof(CellView),
+                new PropertyMetadata(0, delegate (DependencyObject o, DependencyPropertyChangedEventArgs args)
+                {
+                    if (o is CellView cv)
+                        if (args.NewValue is bool value) return;
+                }));
+
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register(nameof(Y), typeof(int), typeof(CellView),
+                new PropertyMetadata(0, delegate (DependencyObject o, DependencyPropertyChangedEventArgs args)
+                {
+                    if (o is CellView cv)
+                        if (args.NewValue is bool value) return;
+                }));
+
+        public bool IsSet
+        {
+            get => (bool) GetValue(IsSetProperty);
+            set => SetValue(IsSetProperty, value);
+        }
+
+        public Brush Color
+        {
+            get => (Brush) GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public bool IsHorse {
+            get => (bool) GetValue(IsHorseProperty);
+            set => SetValue(IsHorseProperty, value);
+        }
+
+        public int X {
+            get => (int)GetValue(XProperty);
+            set => SetValue(XProperty, value);
+        }
+        public int Y {
+            get => (int)GetValue(YProperty);
+            set => SetValue(YProperty, value);
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ chessboard/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace chessboard
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly int[][] _horseMoves = new[]
+        {
+            new[] {-2, 1}, new[] {-2, -1},
+            new[] {-1, 2}, new[] {1, 2},
+            new[] {2, -1}, new[] {2, 1},
+            new[] {1, -2}, new[] {-1, -2},
+        };
+
+        private int _step = 1;
+        private int _currentX;
+        private int _currentY;
+
+        private int _currentCell;
+
+        private List<CellView> _cells;
+
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawBoardCell();
+        }
+
+        private void DrawBoardCell()
+        {
+            int x = 1;
+            int y = 8;
+
+            for (int i = 0; i < 64; i++)
+            {
+                if (x > 8)
+                {
+                    x = 1;
+                    y--;
+                }
+
+                var cell = new CellView();
+                cell.SetPosition(x++, y);
+                if ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0))
+                    cell.Color = Brushes.BurlyWood;
+                else cell.Color = Brushes.SaddleBrown;
+
+                BoardGrid.Children.Add(cell);
+            }
+
+            _cells = BoardGrid.Children.OfType<CellView>().ToList();
+        }
+
+        private void GenerateNewPosition()
+        {
+            _currentCell = new Random().Next(0, 64);
+            var firstCell = (CellView)BoardGrid.Children[_currentCell];
+            firstCell.IsHorse = true;
+            firstCell.IsSet = true;
+            firstCell.Text = "H";
+        }
+
+        private void ClearAll()
+        {
+            _step = 1;
+            BoardGrid.Children.Clear();
+            DrawBoardCell();
+        }
+
+        private void Calculated()
+        {
+
+            _currentX = _cells[_currentCell].X;
+            _currentY = _cells[_currentCell].Y;
+
+            _timer = new DispatcherTimer();
+            _timer.Tick += new EventHandler(MoveHandler);
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+
+            _timer.Start();
+        }
+
+        private void MoveHandler(object sender, EventArgs e)
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (_step == 63) { _timer.Stop(); StartBtn.IsEnabled = true; }
+            
+            var possibleMoves = GetPossibleMoves(_horseMoves, _currentX, _currentY);
+
+            var nextPossibleMovesCount = new List<int>();
+            foreach (CellView cellView in possibleMoves)
+            {
+                int x = cellView.X;
+                int y = cellView.Y;
+                nextPossibleMovesCount.Add(GetPossibleMoves(_horseMoves, x, y).Count);
+            }
+
+            int index = nextPossibleMovesCount.IndexOf(nextPossibleMovesCount.Min());
+            _cells[_currentCell].IsHorse = false;
+            _cells[_currentCell].Text = (_step++).ToString();
+            StepTextLog.Text = _step.ToString();
+            // set new cell
+            CellView nextCell = possibleMoves[index];
+            nextCell.IsHorse = true;
+            nextCell.IsSet = true;
+            nextCell.Text = "H";
+
+            _currentX = nextCell.X;
+            _currentY = nextCell.Y;
+
+            _currentCell = _cells.IndexOf(nextCell);
+        }
+
+        private List<CellView> GetPossibleMoves(int[][] horseMovies, int currentX, int currentY)
+        {
+            List<CellView> cells = new List<CellView>();
+            foreach (int[] move in horseMovies)
+            {
+                int x = currentX + move[0];
+                int y = currentY + move[1];
+                if (x < 9 && x >= 1)
+                    if (y < 9 && y >= 1)
+                    {
+                        var request = _cells.FirstOrDefault(cv => cv.X == x && cv.Y == y);
+                        if (request != null && !request.IsSet)
+                            cells.Add(request);
+                    }
+            }
+
+            return cells;
+        }
+
+        private void StartCalculated(object sender, RoutedEventArgs e)
+        {
+            StartBtn.IsEnabled = false;
+            ClearAll();
+            GenerateNewPosition();
+            Calculated();
+        }
+    }
+}
